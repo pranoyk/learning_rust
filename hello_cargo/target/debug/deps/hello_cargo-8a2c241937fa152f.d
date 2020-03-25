b66dfff7d4508027252a86effc01d91ee2c62e88
pranoyk <pranoy1998k@gmail.com>
2020-03-25 21:01:40 +0530

    generate rust project using using cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-8a2c241937fa152f.d
@@ -0,0 +1,5 @@
+/Users/in-pranoy.kundu/projects/learning_rust/hello_cargo/target/debug/deps/hello_cargo-8a2c241937fa152f: src/main.rs
+
+/Users/in-pranoy.kundu/projects/learning_rust/hello_cargo/target/debug/deps/hello_cargo-8a2c241937fa152f.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/in-pranoy.kundu/projects/learning_rust/hello_cargo/target/debug/hello_cargo.dSYM: /Users/in-pranoy.kundu/projects/learning_rust/hello_cargo/src/main.rs
