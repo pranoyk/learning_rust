ef91a594ae53304fe5c6fce75d6fd71291a219ac
pranoyk <pranoy1998k@gmail.com>
2020-04-05 15:15:26 +0530

    understanding variables - shadowing and mutability

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    // Shadowing
+    let x = 5;
+    println!("The value of x is: {}", x);
+    let x = x + 1;
+    let x = x + 1;
+    let x = x + 1;
+    println!("The value of x is: {}", x);
+
+    // when using let the data type can be changed.
+    // This saves the need to come up with two separate names like 'a_str' and 'a_num'
+    let a = "  string  ";
+    println!("The value of a is: {}", a);
+    let a = a.len();
+    println!("The value of a is: {}", a);
+
+    //Mutability
+    let mut y = 1;
+    println!("The value of y is: {}", y);
+    y = 5;
+    println!("The value of y is: {}", y);
 }
