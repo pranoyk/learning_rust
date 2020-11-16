6752931f584662513c48e996280e2695e375921d
pranoyk <pranoy1998k@gmail.com>
2020-11-16 11:11:12 +0530

    looping with branches

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,16 +1,13 @@
 fn main() {
-    let number = 3;
+    let mut counter = 0;
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
-    }
+    let result = loop {
+        println!("Current count {}", counter);
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
 
-    let condition = true;
-    let number = if condition { 5 } else { 6 };
-
-    println!("The value of number is: {}", number);
-
-    // let number = if condition { 5 } else { "six" }; This fails because the type of variable in if block and else block are different
+    println!("The result is {}", result);
 }
