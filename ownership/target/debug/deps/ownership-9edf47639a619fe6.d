9af4c99b7a6ad1b0b4c94a66570c5ae778ee86bf
Pranoy Kundu <pranoy1998k@gmail.com>
2023-03-04 23:52:20 +0530

    understanding string slices

---
+++ ownership/target/debug/deps/ownership-9edf47639a619fe6.d
@@ -0,0 +1,5 @@
+/home/pranoy/project/learning_rust/ownership/target/debug/deps/ownership-9edf47639a619fe6: src/main.rs
+
+/home/pranoy/project/learning_rust/ownership/target/debug/deps/ownership-9edf47639a619fe6.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/pranoy/project/learning_rust/ownership/target/debug/ownership: /home/pranoy/project/learning_rust/ownership/src/main.rs
