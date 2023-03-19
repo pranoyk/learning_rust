1f529105bd835f7dab5064a9ffc39a6e877b7602
Pranoy Kundu <pranoy1998k@gmail.com>
2023-03-19 19:47:06 +0530

    create modules to undertand the scope and implentation

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,14 +1,17 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+        fn seat_at_table() {}
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
     }
 }
+
+// to access the above code as mod we would have to call `use front_of_house::hosting` and `use front_of_house::serving`
