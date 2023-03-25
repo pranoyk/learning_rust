3479ab32e12d84ef79e871281146a7b24c002341
Pranoy Kundu <pranoy1998k@gmail.com>
2023-03-25 19:09:54 +0530

    add TODO in common-collections to work on later

--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -33,3 +33,9 @@ fn main() {
     let mode = mode(&mut list_of_numbers);
     println!("Mode: {}", mode);
 }
+
+/*
+TODO ::
+    1. Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
+    2. Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
+*/
