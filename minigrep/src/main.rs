7acd016e256eb2c5373df8c86f45dda50ddd0436
Pranoy Kundu <pranoy1998k@gmail.com>
2023-04-11 19:52:52 +0530

    extract config into separate struct and impl a build function around it in order to handle error as well as make code more maintainable

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,37 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filepath = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filepath);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filepath);
 
-    let contents = fs::read_to_string(filepath)
+    let contents = fs::read_to_string(config.filepath)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{content}", content = contents);
+}
+
+struct Config {
+    query: String,
+    filepath: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filepath = args[2].clone();
+
+        Ok(Config { query, filepath })
+    }
 }
