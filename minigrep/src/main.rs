9788c1f8d8e80282ffc3584a82d9a42e26da47b3
Pranoy Kundu <pranoy1998k@gmail.com>
2023-04-11 20:02:08 +0530

    create a separate run function to extract logic from main function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,10 +14,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filepath);
 
-    let contents = fs::read_to_string(config.filepath)
-        .expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
-    println!("With text:\n{content}", content = contents);
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filepath)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
