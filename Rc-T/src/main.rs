6929834a6ff2054c34aa15a1f6e12e97934f58a8
Pranoy Kundu <pranoy1998k@gmail.com>
2023-05-05 19:57:26 +0530

    understanding Rc<T> trait

---
+++ Rc-T/src/main.rs
@@ -0,0 +1,24 @@
+enum List {
+    Cons(i32, Rc<List>),
+    /*
+    We use Rc<List> instead of Box<List> here because we want multiple reference for the same value
+    References are all immutable or read only
+     */
+    Nil,
+}
+
+use std::rc::Rc;
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+}
